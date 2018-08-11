c0672fe448e2e35a4771e40984763f29d18c1aef
danielian1121 <danielian1121@gmail.com>
2018-08-11 16:20:18 +0800

    use static json create List

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var fs = require('fs')
+var router = express.Router()
+
+router.all('*', function (req, res, next) {
+  fs.readFile('./public/data/List.json', function (err, data) {
+    res.locals.posts = JSON.parse(data)
+    next()
+  })
+})
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { data: res.locals.posts });
+})
 
-module.exports = router;
+module.exports = router
