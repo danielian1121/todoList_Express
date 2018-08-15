0eb8dbd373d93856e477fe645f07047062b16492
danielian1121 <danielian1121@gmail.com>
2018-08-15 22:28:26 +0800

    connect mariaDB success

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express')
 var fs = require('fs')
 var router = express.Router()
+const mysql = require('mysql2/promise')
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'sukamo',
+  password: 'suyeehong1121',
+  database: 'todoList'
+});
 
 router.all('*', function (req, res, next) {
   fs.readFile('./public/data/List.json', function (err, data) {
@@ -12,6 +19,16 @@ router.all('*', function (req, res, next) {
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  pool.getConnection()
+    .then(conn => {
+      const res = conn.query('select * from myList')
+      conn.release()
+      return res;
+    }).then((result) => {
+      console.log(result[0])
+    }).catch((err) => {
+      console.log(err) // any of connection time or query time errors from above
+    })
   res.render('index', { data: res.locals.posts });
 })
 
