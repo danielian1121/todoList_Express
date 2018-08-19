9d1e972fc51ac9569cf613110fc903d635744df9
danielian1121 <danielian1121@gmail.com>
2018-08-19 17:01:46 +0800

    receive post data

--- routes/add.js
+++ routes/add.js
@@ -6,5 +6,14 @@ router.get('/', function (req, res, next) {
   //res.send('respond with a resource')
   res.render('addList/addLayout')
 });
+router.post('/', function (req, res, next) {
+  //res.render('addList/addLayout')
+  let body = req.body
+  let title = body.title
+  let content = body.content
+  let date = body.date
+  console.log(`${title} + ${content} + ${date}`)
+  res.send('respond with a resource')
+});
 
 module.exports = router;
