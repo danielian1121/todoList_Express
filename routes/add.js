0cab260502695e26927e896413f1448a65bbaae1
danielian1121 <danielian1121@gmail.com>
2018-08-20 01:33:43 +0800

    add data to DB

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router()
+var mysql = require('../models/sqlPool').pool
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -12,8 +13,26 @@ router.post('/', function (req, res, next) {
   let title = body.title
   let content = body.content
   let date = body.date
-  console.log(`${title} + ${content} + ${date}`)
-  res.send('respond with a resource')
+  addToList(title, content, date, res)
+  //res.send('respond with a resource')
 });
+
+var addToList = function (title, content, date, res) {
+  let sql = {
+    title: title,
+    content: content,
+    time: date
+  }
+  mysql.getConnection()
+    .then(conn => {
+      const res = conn.query('insert into myList set ?', sql)
+      conn.release()
+      return res
+    }).then(result => {
+      res.redirect('/')
+    }).catch(err => {
+      console.log(err)
+    })
+}
 
 module.exports = router;
